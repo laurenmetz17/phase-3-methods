0a2ee9791ccf11b8da6368e9b611fa366714b496
Lauren Metz <Laurenmetz15@gmail.com>
2022-12-27 11:23:16 -0500

    commit tree d45bb92ecdc2a4233f04f2c7db0b9a1602e90255

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name) 
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(num)
+    halved = 0
+    if num.class == Integer
+        halved = num/2
+    else
+        halved = nil
+    end
+    halved
+end
